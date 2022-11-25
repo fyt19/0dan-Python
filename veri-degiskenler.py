ce4191fc3019e112d31d773039b99d701b1f9daf
fyt19 <99129857+fyt19@users.noreply.github.com>
2022-11-26 00:43:14 +0300

    Add files via upload

---
+++ veri-degiskenler.py
@@ -0,0 +1,42 @@
+#3 temel veri tipi vardir int, float, string olmak uzere
+print("---------------")
+a = 5
+print(a)
+b = 1.2
+print(b)
+c = "Merhabalar"
+c = "Hello!"
+print(c)#print(type(a))
+print(type(b))
+print(type(c))
+print("---------------")
+#veri tiplerini birbiri cinsinden yazma
+#int float değere çevirelim
+a = 4
+b = float(a)
+print(type(a))
+print(b)
+print(type(b))
+print("---------------")
+#float int çevirme
+a = 3.2
+b = int(a)
+print(type(a))
+print(b)
+print(type(b))
+print("---------------")
+#int ifadeyi string ifadeye çevirme
+a = 7
+b = str(a)
+print(type(a))
+print(b)
+print(type(b))
+print("---------------")
+#operatorler nelerdir peki? (+,-,*,/,//,%,**) gibi operatorlerimiz vardır
+print(5+5)      #toplama
+print(5-5)      #cikartma
+print(5*5)      #carpim
+print(5/2)      #bolum
+print(12%5)     #mod alama islemi
+print(21//5)    #bölümü gosterir
+print(5**2)     #üs alma
