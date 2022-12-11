6142b96999602ed604de9cd29bd3eca968e931de
fyt19 <99129857+fyt19@users.noreply.github.com>
2022-12-12 00:47:59 +0300

    4.ders

---
+++ main.py
@@ -0,0 +1,35 @@
+#pow üs alma
+a = pow(3,2)
+print(a)
+print(pow(4,4))
+b = pow(-4,3)
+c = pow(-4,4)
+print(b)
+print(c)
+
+print("-------------")
+
+#max-min ksımı->
+
+x = max(1,2,3,4,5,6,7,4.21,7.01)
+print(x)
+y = min(1,2,3,-4.1,5,6,-7)
+print(y)
+
+print("-------------")
+
+#round yuvarlama
+print(round(4.7))
+print(round(4.2))
+print(round(4.5))
+print(round(5.5))
+print(round(6.5))
+
+#buçuklularda orneğin 5.5, 45.5 65.5 gibi sayılarda bir sonraki degere atıyor yani; 6,46,66 gibi
+
+print("-------------")
+
+#abs mutlak deger
+
+print(abs(-74))
+print(abs(74))
